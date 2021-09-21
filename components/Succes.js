53c1e1ac97dfab0d2d1a2564e20e650f7cae3fbb
danildany <76399818+danildany@users.noreply.github.com>
2021-09-21 11:42:48 -0300

    Update Succes.js

--- components/Succes.js
+++ components/Succes.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import { Icon } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
 const Succes = () => {
   return (
-    <View>
-      <Text>pai vamos</Text>
-    </View>
+    <SafeAreaView style={tw`flex flex-col  items-center`}>
+      <Text style={tw`text-center py-9  text-xl`}>We're going</Text>
+      <Icon
+        style={tw`bg-green-500 rounded-full p-5 w-20 `}
+        name="done"
+        color="black"
+        size={40}
+      />
+    </SafeAreaView>
   );
 };
 
